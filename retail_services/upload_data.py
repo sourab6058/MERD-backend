5cea293d68286caa09e52e6909d92bb7a6575d20
sourab6058 <sourab1236kumar@gmail.com>
2021-11-08 15:08:30 +0530

    reports

--- retail_services/upload_data.py
+++ retail_services/upload_data.py
@@ -1,12 +1,16 @@
 from django.http import JsonResponse
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+from wsgiref.util import FileWrapper
 from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 
 import xlrd
 from tablib import Dataset
 import re
+import os
 
 from .submodels.category_expense import CategoryExpense
 from .submodels.user_profile import UserProfile
@@ -28,39 +32,41 @@ import logging
 def expense_upload(request):
     if request.method == 'GET':
         return HttpResponse("True")
-    if request.method=='POST':
-        dataset=Dataset()
-        data=request.data
+    if request.method == 'POST':
+        dataset = Dataset()
+        data = request.data
         new_file = data.get('file')
         book = xlrd.open_workbook(file_contents=new_file.read())
 
         sheet = book.sheet_by_index(0)
-        #headers
-        header_list=[]
-        headers=sheet.row(0)
+        # headers
+        header_list = []
+        headers = sheet.row(0)
         for header_id, header in enumerate(headers):
-            header_list.append({"header_id":header_id, "header":header.value})
+            header_list.append(
+                {"header_id": header_id, "header": header.value})
         count = 0
-        for rowid in range(1,sheet.nrows):
+        for rowid in range(1, sheet.nrows):
             try:
-                row=sheet.row(rowid)
-                category_data=[]
-                subcategory_data=[]
-                subsubcategory_data=[]
+                row = sheet.row(rowid)
+                category_data = []
+                subcategory_data = []
+                subsubcategory_data = []
                 for collid, cell in enumerate(row):
                     if collid == 1:
-                        nationality_name=cell.value
-                        nationality_obj = Nationality.objects.get(nationality__iexact=nationality_name)
+                        nationality_name = cell.value
+                        nationality_obj = Nationality.objects.get(
+                            nationality__iexact=nationality_name)
                         nationality_id = nationality_obj.id
                     if collid == 2:
-                        household_member=cell.value
+                        household_member = cell.value
                     if collid == 3:
-                        city=cell.value
+                        city = cell.value
                         city_obj = City.objects.get(city__iexact=city)
                         city_id = city_obj.id
                     if collid == 4:
-                        zone=str(int(cell.value))
-                        zone_id = Zone.objects.get(zone=zone,city=city_obj).id
+                        zone = str(int(cell.value))
+                        zone_id = Zone.objects.get(zone=zone, city=city_obj).id
                     if collid == 6:
                         monthly_income = cell.value
                     if collid == 7:
@@ -71,83 +77,98 @@ def expense_upload(request):
                         # city_id = city.id
                         # print(city_id)
                         count = count + 1
-                        print("count ",count)
+                        print("count ", count)
 
                     for header in header_list:
                         if header['header_id'] == collid:
                             category_header = header['header']
-                            category_in = re.findall(r'\bcategory\b', category_header, re.IGNORECASE)
-                            category_name=None
-                            sub_category_name=None
-                            sub_sub_category_name=None
+                            category_in = re.findall(
+                                r'\bcategory\b', category_header, re.IGNORECASE)
+                            category_name = None
+                            sub_category_name = None
+                            sub_sub_category_name = None
                             if category_in != []:
-                                remove_header_tag = re.compile(re.escape('category '), re.IGNORECASE)
-                                category_name = remove_header_tag.sub('',category_header)
+                                remove_header_tag = re.compile(
+                                    re.escape('category '), re.IGNORECASE)
+                                category_name = remove_header_tag.sub(
+                                    '', category_header)
                                 category_name = category_name.lstrip()
                                 category_name = category_name.rstrip()
-                                category_id=Category.objects.get(name__iexact=category_name).id
-                                category_data.append({"id":category_id,"expense":cell.value})
+                                category_id = Category.objects.get(
+                                    name__iexact=category_name).id
+                                category_data.append(
+                                    {"id": category_id, "expense": cell.value})
 
-                            sub_category_header=header['header']
-                            subcategory_in = re.findall(r'^\bsub_category\b', sub_category_header, re.IGNORECASE)
+                            sub_category_header = header['header']
+                            subcategory_in = re.findall(
+                                r'^\bsub_category\b', sub_category_header, re.IGNORECASE)
                             if subcategory_in != []:
-                                remove_header_tag = re.compile(re.escape('sub_category '),re.IGNORECASE)
-                                sub_category_name = remove_header_tag.sub('',sub_category_header)
+                                remove_header_tag = re.compile(
+                                    re.escape('sub_category '), re.IGNORECASE)
+                                sub_category_name = remove_header_tag.sub(
+                                    '', sub_category_header)
                                 sub_category_name = sub_category_name.lstrip()
                                 sub_category_name = sub_category_name.rstrip()
-                                subcategory = SubCategory.objects.get(name__iexact = sub_category_name, category_id = category_id)
-                                subcategory_data.append({"id":subcategory.id,"expense":cell.value,"category_id":subcategory.id})
+                                subcategory = SubCategory.objects.get(
+                                    name__iexact=sub_category_name, category_id=category_id)
+                                subcategory_data.append(
+                                    {"id": subcategory.id, "expense": cell.value, "category_id": subcategory.id})
 
-                            sub_sub_category_header=header['header']
-                            subsubcategory_in = re.findall(r'^\bsub_sub_category\b', sub_sub_category_header, re.IGNORECASE)
+                            sub_sub_category_header = header['header']
+                            subsubcategory_in = re.findall(
+                                r'^\bsub_sub_category\b', sub_sub_category_header, re.IGNORECASE)
                             if subsubcategory_in != []:
-                                remove_header_tag = re.compile(re.escape('sub_sub_category '),re.IGNORECASE)
-                                sub_sub_category_name = remove_header_tag.sub('',sub_sub_category_header)
+                                remove_header_tag = re.compile(
+                                    re.escape('sub_sub_category '), re.IGNORECASE)
+                                sub_sub_category_name = remove_header_tag.sub(
+                                    '', sub_sub_category_header)
                                 sub_sub_category_name = sub_sub_category_name.lstrip()
                                 sub_sub_category_name = sub_sub_category_name.rstrip()
-                                subsubcategory = SubSubCategory.objects.get(name__iexact = sub_sub_category_name, sub_category_id=subcategory.id)
-                                subsubcategory_data.append({"id":subsubcategory.id,"expense":cell.value,"subcategory_id":subsubcategory.sub_category_id})
+                                subsubcategory = SubSubCategory.objects.get(
+                                    name__iexact=sub_sub_category_name, sub_category_id=subcategory.id)
+                                subsubcategory_data.append(
+                                    {"id": subsubcategory.id, "expense": cell.value, "subcategory_id": subsubcategory.sub_category_id})
 
                 for category in category_data:
                     expense = CategoryExpense(
-                        category_expense = category['expense'],
-                        year = year,
+                        category_expense=category['expense'],
+                        year=year,
                         month=month,
                         monthly_income=monthly_income,
-                        category_id = category['id'],
+                        category_id=category['id'],
                         city_id=city_id,
                         household_members=household_member,
-                        nationality_id = nationality_id,
-                        zone_id = zone_id
+                        nationality_id=nationality_id,
+                        zone_id=zone_id
                     )
                     expense.save()
 
                 for subcategory in subcategory_data:
                     sub_expense = SubCategoryExpense(
-                        subcategory_expense = subcategory['expense'],
-                        year = year,
+                        subcategory_expense=subcategory['expense'],
+                        year=year,
                         month=month,
                         monthly_income=monthly_income,
-                        sub_category_id = subcategory['id'],
+                        sub_category_id=subcategory['id'],
                         city_id=city_id,
                         household_members=household_member,
-                        nationality_id = nationality_id,
-                        zone_id = zone_id
+                        nationality_id=nationality_id,
+                        zone_id=zone_id
                     )
 
                     sub_expense.save()
 
                 for subsubcategory in subsubcategory_data:
                     sub_sub_expense = SubSubCategoryExpense(
-                        subsubcategory_expense = subsubcategory['expense'],
-                        year = year,
+                        subsubcategory_expense=subsubcategory['expense'],
+                        year=year,
                         month=month,
                         monthly_income=monthly_income,
-                        sub_sub_category_id = subsubcategory['id'],
+                        sub_sub_category_id=subsubcategory['id'],
                         city_id=city_id,
                         household_members=household_member,
-                        nationality_id = nationality_id,
-                        zone_id = zone_id
+                        nationality_id=nationality_id,
+                        zone_id=zone_id
                     )
                     sub_sub_expense.save()
             except Exception as e:
@@ -158,53 +179,55 @@ def expense_upload(request):
                     error = sub_category_name
                 if sub_sub_category_name:
                     error = sub_sub_category_name
-                return JsonResponse({'error':error})
+                return JsonResponse({'error': error})
                 # print("category name ",category_name)
                 # print("sub category name ",sub_category_name)
                 # print("sub sub category name ",sub_sub_category_name)
                 # return HttpResponse(category_name)
 
-    return JsonResponse({'status':True})
+    return JsonResponse({'status': True})
+
 
 @csrf_exempt
 @api_view(['GET', 'POST'])
 def household_upload(request):
     if request.method == 'GET':
         return HttpResponse("True")
-    if request.method=='POST':
+    if request.method == 'POST':
         # try:
-        dataset=Dataset()
-        data=request.data
+        dataset = Dataset()
+        data = request.data
         new_file = data.get('file')
         book = xlrd.open_workbook(file_contents=new_file.read())
 
         sheet = book.sheet_by_index(0)
-        #headers
-        header_list=[]
-        headers=sheet.row(0)
+        # headers
+        header_list = []
+        headers = sheet.row(0)
         for header_id, header in enumerate(headers):
-            header_list.append({"header_id":header_id, "header":header.value})
-    
-        for rowid in range(1,sheet.nrows):
-            row=sheet.row(rowid)
-            nationality_data=[]
+            header_list.append(
+                {"header_id": header_id, "header": header.value})
+
+        for rowid in range(1, sheet.nrows):
+            row = sheet.row(rowid)
+            nationality_data = []
 
             for collid, cell in enumerate(row):
-                if collid == 0:#city
-                    city=cell.value
+                if collid == 0:  # city
+                    city = cell.value
                     print(city)
                     city = City.objects.get(city__iexact=city)
-                    print("city ",city.city)
-                if collid == 1:#zone
-                    zone=str(int(cell.value))
-                    print("zone ",zone)
-                    zone_id = Zone.objects.get(zone=zone,city=city).id
-                if collid == 2:#year
+                    print("city ", city.city)
+                if collid == 1:  # zone
+                    zone = str(int(cell.value))
+                    print("zone ", zone)
+                    zone_id = Zone.objects.get(zone=zone, city=city).id
+                if collid == 2:  # year
                     year = cell.value
-                if collid == 3:#population
-                    population=cell.value
+                if collid == 3:  # population
+                    population = cell.value
                     # zone_id = Zone.objects.get(zone=zone).id
-                if collid == 4:#number of households
+                if collid == 4:  # number of households
                     households = cell.value
                 if collid == 5:
                     family_percent = cell.value
@@ -231,11 +254,13 @@ def household_upload(request):
                         nationality_header = header['header']
                         population_percent = cell.value
                         try:
-                            nationality_id = Nationality.objects.get(nationality__iexact=nationality_header).id
-                            nationality_data.append({"nationality_id":nationality_id, "population":population_percent})
+                            nationality_id = Nationality.objects.get(
+                                nationality__iexact=nationality_header).id
+                            nationality_data.append(
+                                {"nationality_id": nationality_id, "population": population_percent})
                         except Exception as e:
                             pass
-            _household=Household(
+            _household = Household(
                 city=city,
                 year=year,
                 population=population,
@@ -244,8 +269,8 @@ def household_upload(request):
                 family_percent=family_percent,
                 bachelor_percent=bachelor_percent,
                 labourer_percent=labourer_percent,
-                first_half_year = first_half_year,
-                second_half_year = second_half_year
+                first_half_year=first_half_year,
+                second_half_year=second_half_year
             )
             _household.save()
 
@@ -256,8 +281,81 @@ def household_upload(request):
                     nationality_id=nationality['nationality_id']
                 )
                 hn.save()
-            
+
         return HttpResponse("True")
-        
+
         # except Exception as e:
         #     return HttpResponse("error")
+
+
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def city_reports(request):
+    folder = 'reports/city_reports'
+    folder_path = os.path.join(settings.MEDIA_ROOT, folder)
+    if request.method == "GET":
+        filename = request.GET.get('filename', '')
+        if filename == 'all-files':
+            reports = os.listdir(folder_path)
+            print(reports)
+            return JsonResponse({"reports": reports})
+        else:
+            filepath = os.path.join(settings.MEDIA_ROOT, folder, filename)
+            print(filepath)
+            short_report = open(filepath, 'rb')
+            response = HttpResponse(FileWrapper(
+                short_report), content_type='application/pdf')
+            return response
+    if request.method == "POST":
+        data = request.data
+        if data.get("operation") == 'delete':
+            files_to_delete = data.get('filesToDelete')
+            for file in files_to_delete:
+                filepath = os.path.join(settings.MEDIA_ROOT, folder, file)
+                os.remove(filepath)
+            return JsonResponse({"reports": os.listdir(folder_path)})
+
+        else:
+            pdf_file = data.get("file")
+            print(pdf_file.name)
+            fs = FileSystemStorage(location=folder)
+            filename = fs.save(pdf_file.name, pdf_file)
+            file_url = fs.url(filename)
+            print(folder+file_url)
+            return JsonResponse({"filename": pdf_file.name})
+
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def tourist_reports(request):
+    folder = 'reports/tourist_reports'
+    folder_path = os.path.join(settings.MEDIA_ROOT, folder)
+    if request.method == "GET":
+        filename = request.GET.get('filename', '')
+        if filename == 'all-files':
+            reports = os.listdir(folder_path)
+            print(reports)
+            return JsonResponse({"reports": reports})
+        else:
+            filepath = os.path.join(settings.MEDIA_ROOT, folder, filename)
+            print(filepath)
+            short_report = open(filepath, 'rb')
+            response = HttpResponse(FileWrapper(
+                short_report), content_type='application/pdf')
+            return response
+    if request.method == "POST":
+        data = request.data
+        if data.get("operation") == 'delete':
+            files_to_delete = data.get('filesToDelete')
+            for file in files_to_delete:
+                filepath = os.path.join(settings.MEDIA_ROOT, folder, file)
+                os.remove(filepath)
+            return JsonResponse({"reports": os.listdir(folder_path)})
+
+        else:
+            pdf_file = data.get("file")
+            print(pdf_file.name)
+            fs = FileSystemStorage(location=folder)
+            filename = fs.save(pdf_file.name, pdf_file)
+            file_url = fs.url(filename)
+            print(folder+file_url)
+            return JsonResponse({"filename": pdf_file.name})
--- retail_services/urls.py
+++ retail_services/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
-from .upload_data import expense_upload, household_upload
+from .upload_data import expense_upload, household_upload, city_reports, tourist_reports
 
 urlpatterns = [
     path('upload_data/', expense_upload, name='upload-data'),
     path('upload_census_data/', household_upload, name='upload-data-census'),
+    path('city_reports/', city_reports, name='city-reports'),
+    path('tourist_reports/', tourist_reports, name='tourist-reports'),
 
     path('api/filter', views.FilterSecond.as_view(), name='filter-second'),
     path('demographic_info/', views.DemographicInfo.as_view(), name='demographic'),
